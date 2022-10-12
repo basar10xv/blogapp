e1d88c3ddeceff742575ea5ed76ec05c8f6dd3ee
Roel Abasa <basar@10xv.xyz>
2022-10-12 18:41:07 +0800

    integrate klaviyo snippet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
-import {ErrorFallbackProps, ErrorComponent, ErrorBoundary, AppProps} from "@blitzjs/next"
-import {AuthenticationError, AuthorizationError} from "blitz"
+import { ErrorFallbackProps, ErrorComponent, ErrorBoundary, AppProps } from "@blitzjs/next"
+import { AuthenticationError, AuthorizationError } from "blitz"
 import React from "react"
-import {withBlitz} from "app/blitz-client"
+import { withBlitz } from "app/blitz-client"
+import Script from "next/script"
 
-function RootErrorFallback({error}: ErrorFallbackProps) {
+function RootErrorFallback({ error }: ErrorFallbackProps) {
   if (error instanceof AuthenticationError) {
     return <div>Error: You are not authenticated</div>
   } else if (error instanceof AuthorizationError) {
@@ -23,11 +24,16 @@ function RootErrorFallback({error}: ErrorFallbackProps) {
   }
 }
 
-function MyApp({Component, pageProps}: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   const getLayout = Component.getLayout || ((page) => page)
   return (
     <ErrorBoundary FallbackComponent={RootErrorFallback}>
       {getLayout(<Component {...pageProps} />)}
+
+      <Script
+        src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=UcFfxu"
+        strategy="lazyOnload"
+      />
     </ErrorBoundary>
   )
 }
